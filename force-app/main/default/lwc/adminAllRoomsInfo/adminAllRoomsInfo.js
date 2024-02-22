7926c972518859f3d0a75f5b7576d3c9554aa00e
Gaurav Lokhande <gaurravlokhande@gmail.com>
2024-02-22 16:33:36 +0530

    Admin | Mostly Done

--- force-app/main/default/lwc/adminAllRoomsInfo/adminAllRoomsInfo.js
+++ force-app/main/default/lwc/adminAllRoomsInfo/adminAllRoomsInfo.js
@@ -1,3 +1,50 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import ShowAllRooms from '@salesforce/apex/CheckInnAdmin.ShowAllRooms';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class AdminAllRoomsInfo extends LightningElement {}
+export default class AdminAllRoomsInfo extends LightningElement {
+
+    @track isShowModal = false;
+
+    @track AddNewRoomTemplate = false;
+        
+    @track StoreAllRooms;
+
+    connectedCallback() {
+        this.showallroomsAdmin();
+    }
+
+    showallroomsAdmin() {
+        ShowAllRooms()
+            .then((result) => {
+                this.StoreAllRooms = result;
+            console.log(typeof(result));
+        }).catch((error) => {
+            alert(error.body.message)
+        });
+    }
+   
+
+    handleClickOfAddRoom() {
+        this.AddNewRoomTemplate = true;
+        this.isShowModal = true;
+    }
+
+    hideModalBox() {
+        this.isShowModal = false;
+    }
+
+
+    handleSuccess(event) {
+        // Show toast message or any other action upon successful record creation
+        this.isShowModal = false;
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: 'Room Added successfully',
+            variant: 'success',
+        });
+        this.dispatchEvent(toastEvent);
+        
+        // Close the modal or navigate to another page if needed
+    }
+}
